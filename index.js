87568371366d8c943ce6fe4e5cd981ac125273e8
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-02-08 06:18:23 -0800

    Change name.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const parseProperties = (str) => {
-    const lines = str.split("\n");
+const propertiesToJSON = (str) => {
+    const lines = str.split("\n")
     const nonCommentLines = lines.filter(
         (line) =>
             line
@@ -8,18 +8,15 @@ const parseProperties = (str) => {
                 .match(/(\#|\!)/)
                 ? false
                 : line
-    );
-    return nonCommentLines.map(
-        (line) =>
-            line
-                .replace(/(\=)/, ":")
-                .split(":")
-                .map(
-                    ([k, v]) => ({
-                        [k]: v
-                    })
-                )
-    );
-};
+    )
+    return nonCommentLines.map((line) =>
+        line
+            .replace(/(\=)/, ":")
+            .split(":")
+            .map(([k, v]) => ({
+                [k]: v
+            }))
+    )
+}
 
-module.exports = parseProperties;
+module.exports = propertiesToJSON
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join(__dirname, "sample.properties");
-const parseProperties = require("./index.js");
+const fs = require("fs")
+const path = require("path")
+const filePath = path.join(__dirname, "sample.properties")
+const propertiesToJSON = require("./index.js")
 
 fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
     if (!err) {
-        console.log(parseProperties(data));
+        console.log(propertiesToJSON(data))
     } else {
-        console.log(err);
+        console.log(err)
     }
-});
+})
