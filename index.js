ccc9dedca791123a5acd58504e983f4a2ff1e9ad
Ryan McQuen <Ryan.McQuen@surlatable.com>
2018-02-09 07:37:49 -0800

    Add some comments.
    Signed-off-by: Ryan McQuen <Ryan.McQuen@surlatable.com>

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const propertiesToJSON = (str) =>
     str
+        // Concat lines that end with '/'.
         .replace(/\\\n/, "")
+        // Split by line breaks.
         .split("\n")
+        // Remove commented lines:
         .filter(
             (line) =>
                 /(\#|\!)/.test(line.replace(/\s/g, "").slice(0, 1))
                     ? false
                     : line
         )
+        // Create the JSON:
         .reduce((obj, line) => {
             const colonifiedLine = line.replace(/(\=)/, ":")
             const key = colonifiedLine
