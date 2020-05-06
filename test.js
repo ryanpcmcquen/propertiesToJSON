e07e4cffaa6882fc77359f686d326d890de47628
Ryan P. C. McQuen <rpcm@linux.com>
2020-05-06 16:27:59 -0700

    Semicolons for the win.

--- test.js
+++ test.js
@@ -1,35 +1,35 @@
-const fs = require("fs")
-const path = require("path")
-const filePath = path.join(__dirname, "sample.properties")
-const propertiesToJSON = require("./index.js")
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'sample.properties');
+const propertiesToJSON = require('./index.js');
 
-fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
+fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
     if (!err) {
         console.log(`
 -----------------------------
 'sample.properties' contents:
 -----------------------------
-        `)
-        console.log(data)
-        const props = propertiesToJSON(data)
+        `);
+        console.log(data);
+        const props = propertiesToJSON(data);
         console.log(`
 ---------------------------------
 Object of entire properties file:
 ---------------------------------
-        `)
-        console.log(props)
+        `);
+        console.log(props);
         console.log(`
 ----------------------------------------
 'website' key of object (props.website):
 ----------------------------------------
-        `)
-        console.log(props.website)
+        `);
+        console.log(props.website);
         console.log(`
 -----------------
 That's all folks!
 -----------------
-        `)
+        `);
     } else {
-        console.log(err)
+        console.log(err);
     }
-})
+});
