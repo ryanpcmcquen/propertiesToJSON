36499847da508fcb1ba9970dcb7e65ea000139e4
Ryan McQuen <rmcquen@funko.com>
2020-05-06 16:23:07 -0700

    Restore semicolons.
    Signed-off-by: Ryan McQuen <rmcquen@funko.com>

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const propertiesToJSON = (str) =>
-    str
-        // Concat lines that end with '\'.
-        .replace(/\\\n( )*/g, "")
-        // Split by line breaks.
-        .split("\n")
-        // Remove commented lines:
-        .filter(
-            (line) =>
-                /(\#|\!)/.test(line.replace(/\s/g, "").slice(0, 1))
+const propertiesToJSON = (str) => {
+    return (
+        str
+            // Concat lines that end with '\'.
+            .replace(/\\\n( )*/g, '')
+            // Split by line breaks.
+            .split('\n')
+            // Remove commented lines:
+            .filter((line) =>
+                /(\#|\!)/.test(line.replace(/\s/g, '').slice(0, 1))
                     ? false
                     : line
-        )
-        // Create the JSON:
-        .reduce((obj, line) => {
-            const colonifiedLine = line.replace(/(\=)/, ":")
-            const key = colonifiedLine
-                .substring(0, colonifiedLine.indexOf(":"))
-                .trim()
-            const value = colonifiedLine
-                .substring(colonifiedLine.indexOf(":") + 1)
-                .trim()
-            obj[key] = value
-            return obj
-        }, {})
+            )
+            // Create the JSON:
+            .reduce((obj, line) => {
+                const colonifiedLine = line.replace(/(\=)/, ':');
+                const key = colonifiedLine
+                    .substring(0, colonifiedLine.indexOf(':'))
+                    .trim();
+                const value = colonifiedLine
+                    .substring(colonifiedLine.indexOf(':') + 1)
+                    .trim();
+                obj[key] = value;
+                return obj;
+            }, {})
+    );
+};
 
-module.exports = propertiesToJSON
+module.exports = propertiesToJSON;
